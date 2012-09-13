b90df81266f7ee10c942f50e52b0e442666351a4
Zak Peirce <plastikman@gmail.com>
2012-09-13 10:35:39 -0700

    New binaries from NV

--- smba1002/smba1002-vendor-blobs.mk
+++ smba1002/smba1002-vendor-blobs.mk
@@ -19,8 +19,14 @@
 #    vendor/malata/smba1002/proprietary/proprietary/libsecril-client.so:obj/lib/libsecril-client.so
 
 # All the blobs necessary for smba1002
-PRODUCT_COPY_FILES := \
+PRODUCT_COPY_FILES += \
+ $(LOCAL_PATH)/proprietary/bin/nv_hciattach:system/bin/nv_hciattach \
+ $(LOCAL_PATH)/proprietary/bin/nvcap_test:system/bin/nvcap_test \
+ $(LOCAL_PATH)/proprietary/bin/nvcpud:system/bin/nvcpud \
+ $(LOCAL_PATH)/proprietary/bin/nvdmmultidisplay:system/bin/nvdmmultidisplay \
  $(LOCAL_PATH)/proprietary/bin/nvtest:system/bin/nvtest \
+ $(LOCAL_PATH)/proprietary/bin/omx_tests:system/bin/omx_tests \
+ $(LOCAL_PATH)/proprietary/bin/omxplayer2:system/bin/omxplayer2 \
  $(LOCAL_PATH)/proprietary/etc/firmware/arb_test.axf:system/etc/firmware/arb_test.axf \
  $(LOCAL_PATH)/proprietary/etc/firmware/load_test0.axf:system/etc/firmware/load_test0.axf \
  $(LOCAL_PATH)/proprietary/etc/firmware/load_test1.axf:system/etc/firmware/load_test1.axf \
@@ -34,36 +40,28 @@ PRODUCT_COPY_FILES := \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvavp_os_eff00000.bin:system/etc/firmware/nvavp_os_eff00000.bin \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvavp_vid_ucode.bin:system/etc/firmware/nvavp_vid_ucode.bin \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvavp_vid_ucode_alt.bin:system/etc/firmware/nvavp_vid_ucode_alt.bin \
- $(LOCAL_PATH)/proprietary/etc/firmware/nvddk_audiofx_core.axf:system/etc/firmware/nvddk_audiofx_core.axf \
- $(LOCAL_PATH)/proprietary/etc/firmware/nvddk_audiofx_transport.axf:system/etc/firmware/nvddk_audiofx_transport.axf \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvmm_aacdec.axf:system/etc/firmware/nvmm_aacdec.axf \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvmm_adtsdec.axf:system/etc/firmware/nvmm_adtsdec.axf \
- $(LOCAL_PATH)/proprietary/etc/firmware/nvmm_audiomixer.axf:system/etc/firmware/nvmm_audiomixer.axf \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvmm_h264dec.axf:system/etc/firmware/nvmm_h264dec.axf \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvmm_h264dec2x.axf:system/etc/firmware/nvmm_h264dec2x.axf \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvmm_jpegdec.axf:system/etc/firmware/nvmm_jpegdec.axf \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvmm_jpegenc.axf:system/etc/firmware/nvmm_jpegenc.axf \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvmm_manager.axf:system/etc/firmware/nvmm_manager.axf \
- $(LOCAL_PATH)/proprietary/etc/firmware/nvmm_mp2dec.axf:system/etc/firmware/nvmm_mp2dec.axf \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvmm_mp3dec.axf:system/etc/firmware/nvmm_mp3dec.axf \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvmm_mpeg2dec.axf:system/etc/firmware/nvmm_mpeg2dec.axf \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvmm_mpeg4dec.axf:system/etc/firmware/nvmm_mpeg4dec.axf \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvmm_reference.axf:system/etc/firmware/nvmm_reference.axf \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvmm_service.axf:system/etc/firmware/nvmm_service.axf \
- $(LOCAL_PATH)/proprietary/etc/firmware/nvmm_sorensondec.axf:system/etc/firmware/nvmm_sorensondec.axf \
- $(LOCAL_PATH)/proprietary/etc/firmware/nvmm_sw_mp3dec.axf:system/etc/firmware/nvmm_sw_mp3dec.axf \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvmm_vc1dec.axf:system/etc/firmware/nvmm_vc1dec.axf \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvmm_vc1dec_2x.axf:system/etc/firmware/nvmm_vc1dec_2x.axf \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvmm_wavdec.axf:system/etc/firmware/nvmm_wavdec.axf \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvmm_wmadec.axf:system/etc/firmware/nvmm_wmadec.axf \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvmm_wmaprodec.axf:system/etc/firmware/nvmm_wmaprodec.axf \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvrm_avp.bin:system/etc/firmware/nvrm_avp.bin \
- $(LOCAL_PATH)/proprietary/etc/firmware/nvrm_avp_00001000.bin:system/etc/firmware/nvrm_avp_00001000.bin \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvrm_avp_0ff00000.bin:system/etc/firmware/nvrm_avp_0ff00000.bin \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvrm_avp_8e000000.bin:system/etc/firmware/nvrm_avp_8e000000.bin \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvrm_avp_9e000000.bin:system/etc/firmware/nvrm_avp_9e000000.bin \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvrm_avp_be000000.bin:system/etc/firmware/nvrm_avp_be000000.bin \
- $(LOCAL_PATH)/proprietary/etc/firmware/nvrm_avp_e0000000.bin:system/etc/firmware/nvrm_avp_e0000000.bin \
  $(LOCAL_PATH)/proprietary/etc/firmware/nvrm_avp_eff00000.bin:system/etc/firmware/nvrm_avp_eff00000.bin \
  $(LOCAL_PATH)/proprietary/etc/firmware/transport_stress.axf:system/etc/firmware/transport_stress.axf \
  $(LOCAL_PATH)/proprietary/lib/libardrv_dynamic.so:system/lib/libardrv_dynamic.so \
@@ -72,22 +70,17 @@ PRODUCT_COPY_FILES := \
  $(LOCAL_PATH)/proprietary/lib/libnvasfparserhal.so:system/lib/libnvasfparserhal.so \
  $(LOCAL_PATH)/proprietary/lib/libnvaviparserhal.so:system/lib/libnvaviparserhal.so \
  $(LOCAL_PATH)/proprietary/lib/libnvavp.so:system/lib/libnvavp.so \
+ $(LOCAL_PATH)/proprietary/lib/libnvcap.so:system/lib/libnvcap.so \
+ $(LOCAL_PATH)/proprietary/lib/libnvcap_video.so:system/lib/libnvcap_video.so \
+ $(LOCAL_PATH)/proprietary/lib/libnvcapclk.so:system/lib/libnvcapclk.so \
  $(LOCAL_PATH)/proprietary/lib/libnvcontrol_jni.so:system/lib/libnvcontrol_jni.so \
  $(LOCAL_PATH)/proprietary/lib/libnvcpud.so:system/lib/libnvcpud.so \
  $(LOCAL_PATH)/proprietary/lib/libnvcpud_client.so:system/lib/libnvcpud_client.so \
  $(LOCAL_PATH)/proprietary/lib/libnvddk_2d.so:system/lib/libnvddk_2d.so \
  $(LOCAL_PATH)/proprietary/lib/libnvddk_2d_v2.so:system/lib/libnvddk_2d_v2.so \
- $(LOCAL_PATH)/proprietary/lib/libnvddk_audiofx.so:system/lib/libnvddk_audiofx.so \
- $(LOCAL_PATH)/proprietary/lib/libnvdispatch_helper.so:system/lib/libnvdispatch_helper.so \
  $(LOCAL_PATH)/proprietary/lib/libnvdispmgr_d.so:system/lib/libnvdispmgr_d.so \
  $(LOCAL_PATH)/proprietary/lib/libnvhdmi3dplay_jni.so:system/lib/libnvhdmi3dplay_jni.so \
  $(LOCAL_PATH)/proprietary/lib/libnvmm.so:system/lib/libnvmm.so \
- $(LOCAL_PATH)/proprietary/lib/libnvmmlite.so:system/lib/libnvmmlite.so \
- $(LOCAL_PATH)/proprietary/lib/libnvmmlite_audio.so:system/lib/libnvmmlite_audio.so \
- $(LOCAL_PATH)/proprietary/lib/libnvmmlite_image.so:system/lib/libnvmmlite_image.so \
- $(LOCAL_PATH)/proprietary/lib/libnvmmlite_msaudio.so:system/lib/libnvmmlite_msaudio.so \
- $(LOCAL_PATH)/proprietary/lib/libnvmmlite_utils.so:system/lib/libnvmmlite_utils.so \
- $(LOCAL_PATH)/proprietary/lib/libnvmmlite_video.so:system/lib/libnvmmlite_video.so \
  $(LOCAL_PATH)/proprietary/lib/libnvmm_asfparser.so:system/lib/libnvmm_asfparser.so \
  $(LOCAL_PATH)/proprietary/lib/libnvmm_audio.so:system/lib/libnvmm_audio.so \
  $(LOCAL_PATH)/proprietary/lib/libnvmm_aviparser.so:system/lib/libnvmm_aviparser.so \
@@ -99,12 +92,16 @@ PRODUCT_COPY_FILES := \
  $(LOCAL_PATH)/proprietary/lib/libnvmm_msaudio.so:system/lib/libnvmm_msaudio.so \
  $(LOCAL_PATH)/proprietary/lib/libnvmm_parser.so:system/lib/libnvmm_parser.so \
  $(LOCAL_PATH)/proprietary/lib/libnvmm_service.so:system/lib/libnvmm_service.so \
- $(LOCAL_PATH)/proprietary/lib/libnvmm_tracklist.so:system/lib/libnvmm_tracklist.so \
  $(LOCAL_PATH)/proprietary/lib/libnvmm_utils.so:system/lib/libnvmm_utils.so \
  $(LOCAL_PATH)/proprietary/lib/libnvmm_vc1_video.so:system/lib/libnvmm_vc1_video.so \
  $(LOCAL_PATH)/proprietary/lib/libnvmm_video.so:system/lib/libnvmm_video.so \
- $(LOCAL_PATH)/proprietary/lib/libnvmm_vp6_video.so:system/lib/libnvmm_vp6_video.so \
  $(LOCAL_PATH)/proprietary/lib/libnvmm_writer.so:system/lib/libnvmm_writer.so \
+ $(LOCAL_PATH)/proprietary/lib/libnvmmlite.so:system/lib/libnvmmlite.so \
+ $(LOCAL_PATH)/proprietary/lib/libnvmmlite_audio.so:system/lib/libnvmmlite_audio.so \
+ $(LOCAL_PATH)/proprietary/lib/libnvmmlite_image.so:system/lib/libnvmmlite_image.so \
+ $(LOCAL_PATH)/proprietary/lib/libnvmmlite_msaudio.so:system/lib/libnvmmlite_msaudio.so \
+ $(LOCAL_PATH)/proprietary/lib/libnvmmlite_utils.so:system/lib/libnvmmlite_utils.so \
+ $(LOCAL_PATH)/proprietary/lib/libnvmmlite_video.so:system/lib/libnvmmlite_video.so \
  $(LOCAL_PATH)/proprietary/lib/libnvodm_dtvtuner.so:system/lib/libnvodm_dtvtuner.so \
  $(LOCAL_PATH)/proprietary/lib/libnvodm_hdmi.so:system/lib/libnvodm_hdmi.so \
  $(LOCAL_PATH)/proprietary/lib/libnvodm_imager.so:system/lib/libnvodm_imager.so \
@@ -116,7 +113,6 @@ PRODUCT_COPY_FILES := \
  $(LOCAL_PATH)/proprietary/lib/libnvos.so:system/lib/libnvos.so \
  $(LOCAL_PATH)/proprietary/lib/libnvparser.so:system/lib/libnvparser.so \
  $(LOCAL_PATH)/proprietary/lib/libnvrm.so:system/lib/libnvrm.so \
- $(LOCAL_PATH)/proprietary/lib/libnvrm_channel.so:system/lib/libnvrm_channel.so \
  $(LOCAL_PATH)/proprietary/lib/libnvrm_graphics.so:system/lib/libnvrm_graphics.so \
  $(LOCAL_PATH)/proprietary/lib/libnvsm.so:system/lib/libnvsm.so \
  $(LOCAL_PATH)/proprietary/lib/libnvstereoutils_jni.so:system/lib/libnvstereoutils_jni.so \
@@ -128,10 +124,13 @@ PRODUCT_COPY_FILES := \
  $(LOCAL_PATH)/proprietary/lib/libnvwsi.so:system/lib/libnvwsi.so \
  $(LOCAL_PATH)/proprietary/lib/libstagefrighthw.so:system/lib/libstagefrighthw.so \
  $(LOCAL_PATH)/proprietary/lib/omxplayer.so:system/lib/omxplayer.so \
- $(LOCAL_PATH)/proprietary/lib/libwvm.so:system/lib/libwvm.so \
- $(LOCAL_PATH)/proprietary/lib/libWVStreamControlAPI_L3.so:system/lib/libWVStreamControlAPI_L3.so \
+ $(LOCAL_PATH)/proprietary/lib/egl/libEGL_perfhud.so:system/lib/egl/libEGL_perfhud.so \
  $(LOCAL_PATH)/proprietary/lib/egl/libEGL_tegra.so:system/lib/egl/libEGL_tegra.so \
+ $(LOCAL_PATH)/proprietary/lib/egl/libGLESv1_CM_perfhud.so:system/lib/egl/libGLESv1_CM_perfhud.so \
  $(LOCAL_PATH)/proprietary/lib/egl/libGLESv1_CM_tegra.so:system/lib/egl/libGLESv1_CM_tegra.so \
+ $(LOCAL_PATH)/proprietary/lib/egl/libGLESv2_perfhud.so:system/lib/egl/libGLESv2_perfhud.so \
  $(LOCAL_PATH)/proprietary/lib/egl/libGLESv2_tegra.so:system/lib/egl/libGLESv2_tegra.so \
  $(LOCAL_PATH)/proprietary/lib/hw/gralloc.tegra.so:system/lib/hw/gralloc.tegra.so \
- $(LOCAL_PATH)/proprietary/lib/hw/hwcomposer.tegra.so:system/lib/hw/hwcomposer.tegra.so
+ $(LOCAL_PATH)/proprietary/lib/hw/hwcomposer.tegra.so:system/lib/hw/hwcomposer.tegra.so \
+ $(LOCAL_PATH)/proprietary/lib/libwvm.so:system/lib/libwvm.so \
+ $(LOCAL_PATH)/proprietary/lib/libWVStreamControlAPI_L3.so:system/lib/libWVStreamControlAPI_L3.so \
